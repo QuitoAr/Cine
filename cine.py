b66e997b049e569b6052bc95167e276f30951cd8
Quito <smarganaras@hotmail.com>
2025-04-10 00:02:41 -0300

    Iniciando trabajo sobre Directores

--- cine.py
+++ cine.py
@@ -1,10 +1,21 @@
 from PyQt5.QtWidgets import QApplication
 from gui.login import Login
+import sys
+import os
+from gui import login
+sys.path.append(os.path.join(os.path.dirname(__file__), 'gui'))
+
 
 class Cine():
     def __init__(self):
-        self.app = QApplication([])
+        self.app = QApplication(sys.argv)
         self.login = Login()
         
         self.app.exec_()
-        
+
+    if __name__ == "__main__":
+
+        app = QApplication(sys.argv)
+        login = Login()
+        login.show()
+        sys.exit(app.exec_())
