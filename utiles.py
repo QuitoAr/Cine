bc7dfb33cdf343c494bb0fd40c957e02fde919a3
Quito <smarganaras@hotmail.com>
2025-04-23 19:09:38 -0300

    Refactorizar la función recurso_relativo para mejorar la gestión de rutas y agregar soporte para entornos empaquetados. Añadir archivo app.spec para la configuración de PyInstaller.

--- utiles.py
+++ utiles.py
@@ -2,15 +2,11 @@
 import sys
 import os
 
-# Detectar si se está ejecutando desde PyInstaller
-if getattr(sys, 'frozen', False):
-    BASE_DIR = sys._MEIPASS
-else:
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
 def recurso_relativo(ruta_relativa):
-    """
-    Retorna la ruta absoluta al recurso, ya sea desde el entorno normal
-    o desde el entorno empaquetado con PyInstaller.
-    """
-    return os.path.join(BASE_DIR, ruta_relativa)
+    """Devuelve la ruta absoluta al recurso, considerando si está empaquetado."""
+    if hasattr(sys, '_MEIPASS'):
+        base_path = sys._MEIPASS
+    else:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, ruta_relativa)
+
